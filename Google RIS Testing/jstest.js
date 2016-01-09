1e72247b6f909991aef2fca5ae1b7495b4e819ee
Joe Azar-Williams <flatflop@gmail.com>
2016-01-09 16:56:02 -0500

    Update jstest.js

--- Google RIS Testing/jstest.js
+++ Google RIS Testing/jstest.js
@@ -1,1 +1,8 @@
-hello
+var success = function (data){
+  console.log("response recieved");
+  $("main").html(data);
+}
+$.get({
+  url: "https://www.googleapis.com/customsearch/v1?key=AIzaSyAlmwkIlES-eC1J5M7EXnNln9YSJkAG5qw&cx=015086766151602267019:nwjehdn1jq8&q=book",
+  success: success
+});
